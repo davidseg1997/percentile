5e884a72a2797cbb4ce014d98c4069235c58a607
Your Name <your@email.com>
2023-05-01 11:15:59 -0700

    Updating the example

--- R/percentile_max.R
+++ R/percentile_max.R
@@ -7,10 +7,13 @@
 #' @export
 #'
 #' @examples
-#' Calculates the percentile rank max if the 'best' is 'max'. Otherwise, it calculates the percentile of the minimum.
-#' tot_table <- select(x, county_id, asbest, index_of_disparity, total_rate) #%>%
-#'tot_table$disparity_pctile <- p_rank_max(tot_table$index_of_disparity)
-#'tot_table$performance_pctile <- ifelse(tot_table$asbest == 'max', p_rank_max(tot_table$total_rate), p_rank_min(tot_table$total_rate))
+#' Calculates the percentile rank max if the 'best' is 'max'.
+#'#data <- data.frame (county_id= c("06037", "06001", "06113", "06101", "06091"),
+#'county_name = c("Los Angeles", "Alameda", "Yolo", "Sutter", "Sierra"),
+#'total_rate = c("235.70", "440.03", "248.99", "182.93", "192.55"))
+#'  data$performance_pctile <-  p_rank_max(data$total_rate)
+
+
 
 p_rank_max <- function(a){
   r<-rank(a)/sum(!is.na(a)) * 100
